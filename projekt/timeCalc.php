b5d741dcb8dbdc93e23aead85a39df5478368d4f
Sigurd00 <j.skadborg00@gmail.com>
2018-02-28 12:35:25 +0100

    initial comit

---
+++ projekt/timeCalc.php
@@ -0,0 +1,28 @@
+function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+    
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+    
+        $string = array(
+            'y' => 'år',
+            'm' => 'måneder',
+            'w' => 'uger',
+            'd' => 'dage',
+            'h' => 'timer',
+            'i' => 'minutter',
+            's' => 'sekunder',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? '' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+    
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' siden' : 'Lige nu';
+    }
